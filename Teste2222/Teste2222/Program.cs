08b56e565ddb88ec29e59c5416cc8270ed5fada3
Dhml32 <92448556+Dhml32@users.noreply.github.com>
2024-11-25 19:08:26 +0300

    Add files via upload

---
+++ Teste2222/Teste2222/Program.cs
@@ -0,0 +1,102 @@
+﻿/*using System;
+
+class Program 
+{
+    static void Main() 
+    {
+        string name = "Abdulrahman";
+        int age = 19;
+        double favorite_number = 7;
+
+        Console.WriteLine($"Hello my name is {name} I am {age} years old, and my favoeite number is {favorite_number}" );
+    }
+}*/
+
+/*using System;
+using System.Collections.Specialized;
+
+class Program 
+{
+    static void Main() 
+    {
+       Console.WriteLine("Enter Your age: ");
+       int age =int.Parse(Console.ReadLine());
+
+       if (age < 12) 
+       {
+         Console.WriteLine("You are a child ");
+       } else if ( age >= 12 && age <= 17)
+       {
+        Console.WriteLine("You are a teenager ");
+       } else 
+       {
+        Console.WriteLine("You are a adult");
+       }
+    }
+}*/
+
+/*using System;
+class Program 
+{
+    static void Main() 
+    {
+        Console.WriteLine("Enter a number between 1 and 12: ");
+        int user = int.Parse(Console.ReadLine());
+
+        string[] month = {"january", "Fabruary"};
+
+        switch (month) 
+        {
+            case month: (user == 1);
+        }
+    
+    
+
+    }
+}*/
+
+
+/*using System;
+public class book
+{
+   public string Title;
+   public string Author;
+   public double Price;
+   public  book() 
+   {
+
+   } 
+    public string PrintDetails() 
+   {
+      Console.WriteLine($"The Title is: {Title} The Autthor is: ")
+   }
+
+   public class Program 
+   {
+    static void Main() 
+    {
+        book book = new book();
+        book.Title = "The wolve";
+        book.Author = "Abdulrahman";
+        book.Price = 9.99;
+
+        book.PrintDetails();
+    }
+   }
+}
+
+
+/*using System;
+
+class Program 
+{
+    static void Main() 
+    {
+        Console.WriteLine("Enter 5 number");
+        int user = int.Parse(Console.ReadLine());
+        int[] numbers = {0,0,0,0,0};
+        Console.WriteLine(numbers[user]);
+
+
+    }
+}*/
